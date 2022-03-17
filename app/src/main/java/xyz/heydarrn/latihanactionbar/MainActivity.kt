6c06723aaf655e7c4a9d62fe55396ab4309ca590
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2022-03-17 08:34:44 +0700

    melengkapi latihan option menu pada action bar

--- app/src/main/java/xyz/heydarrn/latihanactionbar/MainActivity.kt
+++ app/src/main/java/xyz/heydarrn/latihanactionbar/MainActivity.kt
@@ -1,11 +1,45 @@
 package xyz.heydarrn.latihanactionbar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import xyz.heydarrn.latihanactionbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingMainActivity: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater=menuInflater
+        inflater.inflate(R.menu.option_menu,menu)
+        //return super.onCreateOptionsMenu(menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //return super.onOptionsItemSelected(item)
+        when(item.itemId){
+            R.id.menu_1 -> {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fragment_container,MenuFragment())
+                    .addToBackStack(null)
+                    .commit()
+                return true
+
+            }
+            R.id.menu_2 -> {
+                val bukaMenu=Intent(this,MenuActivity::class.java)
+                startActivity(bukaMenu)
+                return true
+            }
+            else -> return true
+        }
     }
 }
---
+++ app/src/main/java/xyz/heydarrn/latihanactionbar/MenuActivity.kt
@@ -0,0 +1,14 @@
+package xyz.heydarrn.latihanactionbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+
+
+}
